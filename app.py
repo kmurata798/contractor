8e069f758f12009c82fa6af83678a0afa35135c1
kmurata798 <kmurata798@gmail.com>
2019-10-08 11:08:46 -0700

    mongo added

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def index():
+#     """Return homepage."""
+#     return render_template('home.html', msg='Flask is Cool!!')
+
+
+# playlists = [
+#     {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+#     {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+# ]
+
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
-
-
-playlists = [
-    {'title': 'Cat Videos', 'description': 'Cats acting weird'},
-    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
-]
-
-
-@app.route('/playlists')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 
 if __name__ == '__main__':
