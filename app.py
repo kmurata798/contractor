55cb79aa9477991163e135d12831a242ecd42892
kmurata798 <kmurata798@gmail.com>
2019-10-08 10:31:43 -0700

    added playlists index.html

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ def index():
     return render_template('home.html', msg='Flask is Cool!!')
 
 
+playlists = [
+    {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+]
+
+
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
