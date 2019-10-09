e8ef5f1f7d62e5bd456fdf855e8387d40f1c73e5
kmurata798 <kmurata798@gmail.com>
2019-10-08 20:53:32 -0700

    added vanilla bootstrap

--- test.py
+++ test.py
@@ -78,6 +78,14 @@ class PlaylistsTests(TestCase):
         mock_update.assert_called_with({'_id': sample_playlist_id}, {
                                        '$set': sample_playlist})
 
+    @mock.patch('pymongo.collection.Collection.delete_one')
+    def test_delete_playlist(self, mock_delete):
+        form_data = {'_method': 'DELETE'}
+        result = self.client.post(
+            f'/playlists/{sample_playlist_id}/delete', data=form_data)
+        self.assertEqual(result.status, '302 FOUND')
+        mock_delete.assert_called_with({'_id': sample_playlist_id})
+
 
 if __name__ == '__main__':
     unittest_main()
