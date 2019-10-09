b88aa965c59f1bab28f8b06d5ad0c586256d91e7
kmurata798 <kmurata798@gmail.com>
2019-10-09 11:10:22 -0700

    comong

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27018/contractor')
 
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
